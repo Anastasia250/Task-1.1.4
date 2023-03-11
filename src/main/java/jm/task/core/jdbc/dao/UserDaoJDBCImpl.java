8e7f662784ebf935b6dacc2659778521dd02693a
Anastasia <harchenko.a99@gmail.com>
2023-03-11 13:23:52 +0300

    добавление CRUD операций

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,111 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    Util util = new Util();
+    User user = new User();
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
+        PreparedStatement preparedStatement;
 
+        try {
+            preparedStatement = util.getConnection().prepareStatement("CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START WITH 1 INCREMENT BY 1;\n" +
+                    "\n" +
+                    "CREATE TABLE users\n" +
+                    "(\n" +
+                    "    id       BIGINT NOT NULL,\n" +
+                    "    name     VARCHAR(255),\n" +
+                    "    lastName VARCHAR(255),\n" +
+                    "    age      SMALLINT,\n" +
+                    "    CONSTRAINT pk_users PRIMARY KEY (id)\n" +
+                    ");");
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            dropUsersTable();
+            createUsersTable();
+        }
     }
 
     public void dropUsersTable() {
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = util.getConnection().prepareStatement("drop table users");
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException throwables) {
+            createUsersTable();
+            dropUsersTable();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = util.getConnection().prepareStatement("INSERT INTO users (id, name, lastName, age) VALUES (nextval('hibernate_sequence'),?, ?, ?);");
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User с именем - " + name + " добавлен в базу данныых");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = util.getConnection().prepareStatement("delete from users where id = ?");
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new LinkedList<>();
+
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = util.getConnection().prepareStatement("select * from users");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+                list.add(user);
+
+                System.out.println(user);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = util.getConnection().prepareStatement("Truncate table users");
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private  Connection connection;
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "5136962216";
+    public Util() {
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
 }
+
